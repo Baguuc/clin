b8e01209ccea7e0585eccecb1987b131c3122a92
Jan Żukowski <janzukowski@proton.me>
2025-07-04 17:33:41 +0200

    update: added fixed width for progress_bar component

--- src/components/progress_bar.rs
+++ src/components/progress_bar.rs
@@ -1,18 +1,28 @@
 pub fn progress_bar(max: usize, now: usize) {
     use std::io::{self, Write};
     use colored::Colorize;
-    
-    print!("\r");
 
-    for _ in 0..max-1 {
+    let base = 60f32;
+    
+    let now = if now == 0 { 1 } else { now } as f32;
+    let max = (max - 1) as f32;
+
+    let curr_perc = now / max;
+    let curr_steps = (base * curr_perc) as usize;
+    
+    let base = base as usize;
+         
+    print!("\r");
+    
+    for _ in 0..base {
         print!("{}", "―".red());
     }
 
     print!("\r"); 
 
-    for _ in 0..now {
+    for _ in 0..curr_steps {
         print!("{}", "█".green());
     }
 
-    io::stdout().flush();
+    let _ = io::stdout().flush();
 }
