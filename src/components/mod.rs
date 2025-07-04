295879098e3c0d1e3b846aad2083010aa6ed1959
Jan Żukowski <janzukowski@proton.me>
2025-07-04 15:56:39 +0200

    update: added paragraph component

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,3 +1,5 @@
+pub mod paragraph;
 pub mod progress_bar;
 
 pub use progress_bar::progress_bar;
+pub use paragraph::paragraph;
---
+++ src/components/paragraph.rs
@@ -0,0 +1,10 @@
+pub fn paragraph(text: impl std::fmt::Display, bold: bool, underline: bool) {
+    use colored::Colorize;
+    
+    let mut text = text.to_string();
+    
+    if bold { text = text.bold().to_string(); };
+    if underline { text = text.underline().to_string(); };
+
+    println!(" {}", text);
+}
